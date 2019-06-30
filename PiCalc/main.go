f7af712d726ac950d100bafb2dfe20d302cf4e4d
dexterchan <dexterchan@gmail.com>
2019-06-30 15:25:45 +0800

    pi calc

---
+++ PiCalc/main.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+type PITerm interface {
+	setTerm(int) PITerm
+	//getTerm()
+	calculateTerm() float64
+	getFinalCalculation() func(float64) float64
+}
+
+type Nilakantha struct {
+	term      int
+	isOddTerm bool
+}
+
+func (n Nilakantha) setTerm(term int) PITerm {
+	n.term = term
+	n.isOddTerm = (term%2 != 0)
+	return n
+}
+
+/*
+func (n Nilakantha) getTerm() int {
+	return n.term
+}*/
+func (n Nilakantha) calculateTerm() float64 {
+	var d float64
+	var factor float64
+	d = (2.0) * (float64(n.term) + 1.0)
+	factor = d * (d + 1.0) * (d + 2.0)
+	ret := 4.0 / factor
+	if n.isOddTerm {
+		ret = ret * -1
+	}
+	return ret
+}
+
+func (n Nilakantha) getFinalCalculation() func(float64) float64 {
+	return func(x float64) float64 {
+		return 3.0 + x
+	}
+}
+
+func createTerm(numOfStep int) []PITerm {
+	terms := make([]PITerm, numOfStep, numOfStep)
+
+	for i := 0; i < numOfStep; i++ {
+		n := Nilakantha{}
+
+		piterm := n.setTerm(i)
+
+		//terms = append(terms, piterm)
+		terms[i] = piterm
+	}
+	return terms
+}
+func executeTerm(terms []PITerm) float64 {
+	var accum float64 = 0.0
+	for _, v := range terms {
+		r := v.calculateTerm()
+		accum += r
+	}
+	finalValueFunc := terms[0].getFinalCalculation()
+	finalValue := finalValueFunc(accum)
+	return finalValue
+}
+
+func main() {
+	terms := createTerm(1000)
+	pi := executeTerm(terms)
+	fmt.Println(pi)
+}
--- section14/main.go
+++ section14/main.go
@@ -12,7 +12,21 @@ func main() {
 		fmt.Println(x)
 	}
 
+	var unfold []int = []int{1, 2, 3}
 	variadic(1, 2, 3, 4, 5, 6)
+	variadic(unfold...)
+	playdefer()
+
+	setupSecretAgent()
+
+	anonymousfunc()
+
+	sqFunc := returnFunc()
+	fmt.Printf("%T sqFunc:%d\n", sqFunc, sqFunc(10))
+
+	demoCallback()
+
+	runScope()
 }
 
 //everything in GO is pass by value
@@ -39,4 +53,136 @@ func variadic(x ...int) {
 		sum += v
 		fmt.Println(i, v, sum)
 	}
+}
+
+func foo() {
+	fmt.Println("foo")
+}
+func bar2() {
+	fmt.Println("bar")
+}
+func playdefer() {
+	defer foo()
+	bar2()
+	fmt.Println("closing playdefer")
+}
+
+type Pet struct {
+	name string
+}
+type Person struct {
+	first string
+	last  string
+}
+
+type SecretAgent struct {
+	Person
+	ltk bool
+}
+
+//attach a method to a type struct
+func (s SecretAgent) speak() {
+	fmt.Println("I am ", s.last, ",", s.first, s.last)
+}
+
+func (p Person) speak() {
+	fmt.Println("Hello, I am ", p.first, p.last)
+}
+
+func setupSecretAgent() {
+	sa1 := SecretAgent{
+		Person: Person{
+			"James",
+			"Bond",
+		},
+		ltk: false,
+	}
+	pp := Person{
+		"Bob",
+		"X",
+	}
+	fmt.Println(sa1)
+	sa1.speak()
+
+	/*pet1 :=
+	Pet{
+		"dog",
+	}*/
+	h := HumanInterface(sa1)
+	fmt.Println(h)
+	interface_polymorphism(sa1)
+	interface_polymorphism(pp)
+	//interface_polymorphism(pet1)
+}
+
+type HumanInterface interface {
+	speak()
+}
+
+func interface_polymorphism(h HumanInterface) {
+
+	switch h.(type) {
+	case Person:
+		fmt.Println("it is person", h.(Person)) //assert it is Person
+	case SecretAgent:
+		fmt.Println("It is secret agent", h.(SecretAgent))
+	default:
+		fmt.Println("it is unknown")
+	}
+}
+
+func anonymousfunc() {
+	a := func(x int) {
+		fmt.Println("Anonymous func run", x)
+	}
+	a(100)
+}
+
+func returnFunc() func(x int) int {
+	return func(x int) int {
+		return x * x
+	}
+}
+
+func demoCallback() {
+	var x = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println("odd sum:", odd(sumUp, x...))
+}
+func sumUp(x ...int) int {
+	accum := 0
+	for _, v := range x {
+		accum += v
+	}
+	return accum
+}
+
+func odd(f func(...int) int, x ...int) int {
+	var temp []int
+	for _, v := range x {
+		if v%2 == 1 {
+			temp = append(temp, v)
+		}
+	}
+	return f(temp...)
+}
+
+func incrementor() func() int {
+	var x = 10
+
+	return func() int {
+		x++
+		return x
+	}
+
+}
+
+func runScope() {
+	a := incrementor()
+	b := incrementor()
+
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
 }
